643ed1f88a09080b52dbb89c8d65241aa02366fa
Damian Janus <damian.janus@gmail.com>
2023-09-13 21:45:17 +0200

    test

--- src/lib/server/pdf/index.ts
+++ src/lib/server/pdf/index.ts
@@ -1,51 +1,41 @@
 import PdfPrinter from 'pdfmake';
 import blobStream, { type IBlobStream } from 'blob-stream';
 import type { TDocumentDefinitions, TFontDictionary } from 'pdfmake/interfaces';
-import path from "node:path";
-import normal from '../../assets/fonts/FuturaPTBook.otf';
-import bold from '../../assets/fonts/FuturaPTBold.otf';
-
-
+import path from 'node:path';
 
 const fonts: TFontDictionary = {
-    Futura: {
-        normal: normal,
-        bold: bold
-    }
+	Futura: {
+		normal: 'public/FuturaPTBook.otf',
+		bold: 'public/FuturaPTBold.otf'
+	}
 };
 const printer = new PdfPrinter(fonts);
 
 async function genInvoicePdf(): Promise<Blob> {
+	const file: TDocumentDefinitions = {
+		pageMargins: [40, 40, 40, 60],
+		content: ['Hello, World!!'],
+		defaultStyle: {
+			font: 'Futura'
+		}
+	};
 
+	return new Promise((resolve, reject) => {
+		const pdf = printer.createPdfKitDocument(file);
 
+		pdf
+			.pipe(blobStream())
+			.on('finish', function (this: IBlobStream) {
+				console.log('Finished generating PDF');
+				resolve(this.toBlob('application/pdf'));
+			})
+			.on('error', (err) => {
+				console.error('err', err);
+				reject(err);
+			});
 
-    const file: TDocumentDefinitions = {
-
-        pageMargins: [40, 40, 40, 60],
-        content: [
-            "Hello, World!!"
-        ],
-        defaultStyle: {
-            font: 'Futura'
-        }
-    };
-
-    return new Promise((resolve, reject) => {
-        const pdf = printer.createPdfKitDocument(file);
-
-        pdf
-            .pipe(blobStream())
-            .on('finish', function (this: IBlobStream) {
-                console.log('Finished generating PDF');
-                resolve(this.toBlob('application/pdf'));
-            })
-            .on('error', (err) => {
-                console.error('err', err);
-                reject(err);
-            });
-
-        pdf.end();
-    });
+		pdf.end();
+	});
 }
 
 export default genInvoicePdf;
